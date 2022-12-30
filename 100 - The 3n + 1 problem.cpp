a8755e90bfcf1366ba1bd276a452831b8c86ed9f
Tasif Hossain Emon <tasifthe2002@gmail.com>
2022-12-30 15:06:57 +0600

    solved

---
+++ 100 - The 3n + 1 problem.cpp
@@ -0,0 +1,40 @@
+//Solved by Tasif Hossain Emon
+//Student of SMUCT //BATCH 29th //ID:221071042
+
+#include<bits/stdc++.h>
+
+using namespace std;
+
+int cl(int n)
+{
+    int count = 1;
+    while (n != 1)
+    {
+        if (n % 2 == 0)
+        {
+            n /= 2;
+        }
+        else
+        {
+            n = 3 * n + 1;
+        }
+        count++;
+    }
+    return count;
+}
+
+int main()
+{
+    int i, j;
+    while (cin >> i >> j)
+    {
+        cout << i << " " << j << " ";
+        int ml = 0;
+        for (int k = min(i, j); k <= max(i, j); k++)
+        {
+            ml = max(ml, cl(k));
+        }
+        cout << ml << endl;
+    }
+    return 0;
+}
