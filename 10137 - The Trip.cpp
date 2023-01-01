e44f1b421517302b636f00958de496d7ce852200
Tasif Hossain Emon <tasifthe2002@gmail.com>
2023-01-02 02:18:40 +0600

    .

---
+++ 10137 - The Trip.cpp
@@ -0,0 +1,37 @@
+//@Solved by Tasif Hossain Emon
+//Student of SMUCT//ID: 221071042
+
+#include<bits/stdc++.h>
+
+using namespace std;
+
+int main()
+{
+    int N;
+    
+    while (cin >> N, N)
+    {
+        vector<int> st(N);
+        for (int i = 0; i < N; ++i)
+        {
+            int dollar, cent;
+            char temp;
+            cin >> dollar >> temp >> cent;
+            st[i] = dollar * 100 + cent;
+        }
+        
+        int sum = accumulate(st.begin(), st.end(), 0);
+        int lowAv = sum / N;
+        int highAv = lowAv + ((sum % N) ? 1 : 0);
+        
+        int sumAbove = accumulate(st.begin(), st.end(), 0,
+            [&](int a, int b) { return a + (b > highAv ? b - highAv : 0); });
+        
+        int sumBelow = accumulate(st.begin(), st.end(), 0,
+            [&](int a, int b) { return a + (b < lowAv ? lowAv - b : 0); });
+        
+        int usedSum = max(sumAbove, sumBelow);
+        
+        cout << '$' << (usedSum / 100) << '.' << setw(2) << setfill('0') << (usedSum % 100) << '\n';
+    }
+}
---
+++ 706 - LC-Display.cpp
@@ -0,0 +1,75 @@
+//Solved by Tasif Hossain Emon
+//Student of SMUCT//ID: 221071042
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int s;
+    char n[20],ch;
+    while (1)
+    {
+        scanf("%d %s",&s,n);
+        if (s==0 && n[0]=='0') break;
+        int l=strlen(n);
+        for (int i=0;i<l;i++)
+        {
+            if (i) printf(" ");
+            printf(" ");
+            if (n[i]=='1' || n[i]=='4') ch=' ';
+            else ch='-';
+            for (int j=0;j<s; j++) printf("%c",ch);
+            printf(" ");
+        }
+        printf("\n");
+        for (int k=0;k<s;k++)
+        {
+            for (int i=0;i<l;i++)
+            {
+                if (i) printf(" ");
+                if (n[i]=='1' || n[i]=='2' || n[i]=='3' || n[i]=='7')
+                printf(" ");
+                else printf("|");
+                for (int j=0;j<s;j++) printf(" ");
+                if (n[i]=='5' || n[i]=='6') printf(" ");
+                else printf("|");
+            }
+            printf("\n");
+        }
+        for (int i=0;i<l;i++)
+        {
+            if (i) printf(" ");
+            printf(" ");
+            if (n[i]=='0' || n[i]=='1' || n[i]=='7') ch=' ';
+            else ch='-';
+            for (int j=0;j<s; j++) printf("%c",ch);
+            printf(" ");
+        }
+        printf("\n");
+        for (int k=0;k<s;k++)
+        {
+            for (int i=0;i<l;i++)
+            {
+                if (i) printf(" ");
+                if (n[i]=='0' || n[i]=='2' || n[i]=='6' ||n[i]=='8')
+                printf("|");
+                else printf(" ");
+                for (int j=0;j<s;j++) printf(" ");
+                if (n[i]=='2') printf(" ");
+                else printf("|");
+            }
+            printf("\n");
+        }
+        for (int i=0;i<l;i++)
+        {
+            if (i) printf(" ");
+            printf(" ");
+            if (n[i]=='1' || n[i]=='4' || n[i]=='7') ch=' ';
+            else ch='-';
+            for (int j=0;j<s; j++) printf("%c",ch);
+            printf(" ");
+        }
+        printf("\n\n");
+    }
+    return 0;
+}
