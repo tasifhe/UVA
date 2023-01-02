a87f9b2eef64c3fb4c5459442a02c20c66739df7
Tasif Hossain Emon <tasifthe2002@gmail.com>
2023-01-02 15:33:53 +0600

    Solve update

--- 10189 - Minesweeper.cpp
+++ 10189 - Minesweeper.cpp
@@ -5,15 +5,22 @@
 
 using namespace std;
 
-void solve_field(vector<string>& field, int num_rows, int num_cols) {
-  for (int row = 0; row < num_rows; row++) {
-    for (int col = 0; col < num_cols; col++) {
-      if (field[row][col] != '*') {
+void solve_field(vector<string>& field, int num_rows, int num_cols)
+{
+  for (int row = 0; row < num_rows; row++)
+  {
+    for (int col = 0; col < num_cols; col++)
+    {
+      if (field[row][col] != '*')
+      {
         int count = 0;
-        for (int i = -1; i <= 1; i++) {
-          for (int j = -1; j <= 1; j++) {
+        for (int i = -1; i <= 1; i++)
+        {
+          for (int j = -1; j <= 1; j++)
+          {
             if (!(i == 0 && j == 0) && 0 <= row + i && row + i < num_rows && 0 <= col + j && col + j < num_cols) {
-              if (field[row + i][col + j] == '*') {
+              if (field[row + i][col + j] == '*')
+              {
                 count++;
               }
             }
@@ -25,17 +32,21 @@ void solve_field(vector<string>& field, int num_rows, int num_cols) {
   }
 }
 
-int main() {
+int main()
+{
   int field_num = 1;
-  while (true) {
+  while (true)
+  {
     int num_rows, num_cols;
     cin >> num_rows >> num_cols;
-    if (num_rows == 0 && num_cols == 0) {
+    if (num_rows == 0 && num_cols == 0)
+    {
       break;
     }
 
     vector<string> field;
-    for (int i = 0; i < num_rows; i++) {
+    for (int i = 0; i < num_rows; i++)
+    {
       string line;
       cin >> line;
       field.push_back(line);
@@ -43,7 +54,8 @@ int main() {
 
     cout << "Field #" << field_num << ":" << endl;
     solve_field(field, num_rows, num_cols);
-    for (const string& row : field) {
+    for (const string& row : field)
+    {
       cout << row << endl;
     }
     cout << endl;
--- 458 - The Decoder.cpp
+++ 458 - The Decoder.cpp
@@ -1,2 +1,18 @@
 //*Solved by Tasif Hossain Emon
 //*Student of SMUCT //BATCH 29th //ID:221071042
+
+#include<bits/stdc++.h>
+
+int main()
+{
+    //freopen("input.txt", "a", stdin);
+    //freopen("output.txt", "b", stdout);
+
+    char c;
+    while ((c = getchar()) != EOF)
+    {
+        if (c != '\n') putchar(c - 7);
+        else putchar('\n');
+    }
+    return 0;
+}
